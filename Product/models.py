621968ff195c46ca7196ec1ab7a7ce43f8579233
akramongitnp <akramsiddikinp@gmail.com>
2022-04-30 10:21:21 +0530

    The Product app with all db models are created

---
+++ Product/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='assets/products')
+    type = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
--- User/models.py
+++ User/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
+from pyexpat import model
+from unicodedata import name
 from django.db import models
-
-# Create your models here.
